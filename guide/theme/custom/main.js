c72ea3b1ee3e304b43e0c53f107dfa5175cfaef1
yexiyue <yexiyue666@qq.com>
2024-03-16 17:41:18 +0800

    docs:modify translate

--- guide/theme/custom/main.js
+++ guide/theme/custom/main.js
@@ -1,5 +1,5 @@
 //设置机器翻译服务通道，直接客户端本身，不依赖服务端 。相关说明参考 http://translate.zvo.cn/43086.html
-translate.service.use("client.edge");
+// translate.service.use("client.edge");
 translate.ignore.class.push("icon-button");
 translate.ignore.class.push("theme-popup");
 //进行翻译
